821aa9b9503010786f06f50aae8cff1a2018a205
Sona Chitchyan <chitchyan.sona@gmail.com>
2017-06-07 13:36:14 +0300

    file transfer commit

--- app/src/main/java/com/example/asus/myapplication/MainActivity.java
+++ app/src/main/java/com/example/asus/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
@@ -20,6 +21,14 @@ import android.widget.Toast;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -27,7 +36,7 @@ import java.util.concurrent.TimeUnit;
 public class MainActivity extends AppCompatActivity implements View.OnKeyListener {
 
     private RadioButton radio_barcode, radio_code, radio_article;
-    private Button clear;
+    private Button clear, add;
     private EditText barcode, count;
     private TextView name_text, count_text, value_text;
     private String hints[] = {"Barcode"};
@@ -45,6 +54,7 @@ public class MainActivity extends AppCompatActivity implements View.OnKeyListene
         barcode = (EditText) findViewById(R.id.barcode);
         count = (EditText) findViewById(R.id.count);
         clear = (Button) findViewById(R.id.clear);
+        add = (Button) findViewById(R.id.add);
         name_text = (TextView) findViewById(R.id.name_text);
         count_text = (TextView) findViewById(R.id.count_text);
         value_text = (TextView) findViewById(R.id.value_text);
@@ -77,15 +87,20 @@ public class MainActivity extends AppCompatActivity implements View.OnKeyListene
             }
         });
 
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Reader reader = new Reader();
+                reader.execute();
+            }
+        });
+
         radio_barcode.setChecked(true);
         barcode.setFocusable(false);
         barcode.setFocusableInTouchMode(false);
         count.setCursorVisible(false);
         barcode.setCursorVisible(false);
 
-
-        count.setOnKeyListener(this);
-        barcode.setOnKeyListener(this);
     }
 
     public void onRadioButtonChecked(View view) {
@@ -125,13 +140,8 @@ public class MainActivity extends AppCompatActivity implements View.OnKeyListene
         View view = getCurrentFocus();
         if (event.getAction() == KeyEvent.ACTION_DOWN) {
             switch (keyCode) {
-                case KeyEvent.KEYCODE_ENTER:
-                    if(view.getId() == R.id.count){
-                    Reader reader = new Reader();
-                    reader.execute();}
-                    break;
                 case 7:
-                    if (!((EditText)view).getText().toString().equals("")) {
+                    if (!((EditText) view).getText().toString().equals("")) {
                         adder(0, view);
                     }
                     break;
@@ -157,17 +167,17 @@ public class MainActivity extends AppCompatActivity implements View.OnKeyListene
                     adder(7, view);
                     break;
                 case 48:
-                    adder(8,view);
+                    adder(8, view);
                     break;
                 case 51:
                     adder(9, view);
                     break;
                 case 67:
-                    String text = ((EditText)view).getText().toString();
+                    String text = ((EditText) view).getText().toString();
                     if (!text.equals("")) {
                         text = text.substring(0, text.length() - 1);
                     }
-                    ((EditText)view).setText(text);
+                    ((EditText) view).setText(text);
                     break;
             }
         }
@@ -175,84 +185,164 @@ public class MainActivity extends AppCompatActivity implements View.OnKeyListene
     }
 
 
+    class Reader extends AsyncTask<String, Void, String> {
+        Gson gson = new Gson();
+        String a = null;
+        String filename = "data.txt";
+        File file;
 
-class Reader extends AsyncTask<String, Void, String> {
-    Gson gson = new Gson();
-    String a = null;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Data data = new Data();
+            boolean exists = false;
+            data.setName("");
+            switch (hints[0]) {
+                case "Barcode":
 
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-        Data data = new Data();
-        data.setName("");
-        switch (hints[0]) {
-            case "Barcode":
-                data.setBarcode(barcode.getText().toString());
-                data.setCode(0);
-                data.setArticle("");
-                break;
-            case "Code":
-                if (!barcode.getText().equals("")) {
-                    data.setBarcode("");
-                    data.setCode(Integer.parseInt(barcode.getText().toString()));
-                    data.setArticle("");
+                    for (Data d : dataList) {
+                        if (d.getBarcode().equals(barcode.getText().toString())) {
+                            int temp = d.getCount();
+                            if (count.getText().toString().equals("")) {
+                                d.setCount(temp + 1);
+                            } else {
+                                d.setCount(temp + Integer.parseInt(count.getText().toString()));
+                            }
+                            exists = true;
+                        }
+                    }
+                    if (!exists) {
+                        data.setBarcode(barcode.getText().toString());
+                        data.setCode(0);
+                        data.setArticle("");
+                        dataList.add(data);
+                    }
+                    break;
+                case "Code":
+                    if (!barcode.getText().equals("")) {
+                        for (Data d : dataList) {
+                            if (d.getCode() == Integer.parseInt(barcode.getText().toString())) {
+                                int temp = d.getCount();
+                                if (count.getText().toString().equals("")) {
+                                    d.setCount(temp + 1);
+                                } else {
+                                    d.setCount(temp + Integer.parseInt(count.getText().toString()));
+                                }
+                                exists = true;
+                            }
+                        }
+                        if (!exists) {
+                            data.setBarcode("");
+                            data.setCode(Integer.parseInt(barcode.getText().toString()));
+                            data.setArticle("");
+                            dataList.add(data);
+                        }
+                    }
+                    break;
+                case "Article":
+                    if (!barcode.getText().equals("")) {
+                        for (Data d : dataList) {
+                            if (d.getArticle().equals(barcode.getText().toString())) {
+                                int temp = d.getCount();
+                                if (count.getText().toString().equals("")) {
+                                    d.setCount(temp + 1);
+                                } else {
+                                    d.setCount(temp + Integer.parseInt(count.getText().toString()));
+                                }
+                                exists = true;
+                            }
+                        }
+                        if (!exists) {
+                            data.setBarcode("");
+                            data.setCode(0);
+                            data.setArticle(barcode.getText().toString());
+                            dataList.add(data);
+                        }
+                    }
+                    break;
+            }
+            if (count.getText().toString().equals("")) {
+                data.setCount(1);
+            } else {
+
+                data.setCount(Integer.parseInt(count.getText().toString()));
+            }
+
+            a = gson.toJson(dataList);
+            if(!isExternalStorageWritable()){
+                Toast.makeText(MainActivity.this, "failed to connect", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                file = new File(Environment.getExternalStorageDirectory(), a);
+            }
+            try {
+                FileOutputStream fos = new FileOutputStream(file);
+                fos.write(a.getBytes());
+                a = "";
+                fos.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String aVoid) {
+            super.onPostExecute(aVoid);
+
+            try {
+                FileInputStream fis = new FileInputStream(filename);
+                DataInputStream dis = new DataInputStream(fis);
+                BufferedReader buff = new BufferedReader(new InputStreamReader(dis));
+                String strline;
+                while ((strline = buff.readLine()) != null){
+                    a = a + strline;
                 }
-                break;
-            case "Article":
-                if (!barcode.getText().equals("")) {
-                    data.setBarcode("");
-                    data.setCode(0);
-                    data.setArticle(barcode.getText().toString());
-                }
-                break;
-        }
-        if (count.getText().toString().equals("")) {
-            data.setCount(1);
-        } else {
+                dis.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-            data.setCount(Integer.parseInt(count.getText().toString()));
+            List<Data> dataList1 = gson.fromJson(a, new TypeToken<ArrayList<Data>>() {
+            }.getType());
+            Data data;
+            if (!dataList.isEmpty()) {
+                data = dataList.get(dataList1.size() - 1);
+                name_text.setText(data.getName());
+                count_text.setText("" + data.getCount() + "/" + data.getCount_db());
+                value_text.setText("" + data.getPrice());
+            }
         }
-        data.setPrice(0.0);
-        dataList.add(data);
-        a = gson.toJson(dataList);
 
     }
-
-    @Override
-    protected String doInBackground(String... params) {
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @Override
-    protected void onPostExecute(String aVoid) {
-        super.onPostExecute(aVoid);
-        List<Data> datas = gson.fromJson(a, new TypeToken<ArrayList<Data>>() {
-        }.getType());
-        Data data;
-        if (!dataList.isEmpty()) {
-            data = dataList.get(datas.size() - 1);
-            name_text.setText(data.getName());
-            count_text.setText("" + data.getCount());
-            value_text.setText("" + data.getPrice());
-        }
-    }
-
-}
 
     public void adder(int i, View v) {
-        String a = ((EditText)v).getText().toString();
-        a +=i;
-        ((EditText)v).setText(a);
+        String a = ((EditText) v).getText().toString();
+        a += i;
+        ((EditText) v).setText(a);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         this.unregisterReceiver(receiver);
+    }
+
+
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
     }
 }
